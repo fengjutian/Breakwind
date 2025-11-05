9924fc840f5d4863a1d2ddc1035d3f5f3c8bea6b
fengjutian <fjutian@foxmail.com>
2025-11-05 23:02:25 +0800

    docs(importer): 为导入器模块添加注释说明
    添加模块级注释和函数级注释，说明导入器模块的功能和各个方法的作用

--- app/src/main/importer.ts
+++ app/src/main/importer.ts
@@ -1,3 +1,8 @@
+/**
+ * 导入器模块
+ * 负责处理文件导入和浏览器历史/书签导入功能
+ * 提供与Electron对话框交互和IPC通信的功能
+ */
 import { dialog } from 'electron'
 
 import { BrowserType } from '@deta/types'
@@ -5,8 +10,13 @@ import { BrowserType } from '@deta/types'
 import { ipcSenders } from './ipcHandlers'
 import { useLogScope } from '@deta/utils'
 
+/** 导入器日志实例 */
 const log = useLogScope('Importer')
 
+/**
+ * 导入文件
+ * 打开文件选择对话框，允许用户选择多个文件进行导入
+ */
 export const importFiles = async () => {
   try {
     const result = await dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] })
@@ -19,6 +29,10 @@ export const importFiles = async () => {
   }
 }
 
+/**
+ * 导入浏览器历史记录
+ * @param type 浏览器类型
+ */
 export const importBrowserHistory = async (type: BrowserType) => {
   try {
     ipcSenders.importBrowserHistory(type)
@@ -27,6 +41,10 @@ export const importBrowserHistory = async (type: BrowserType) => {
   }
 }
 
+/**
+ * 导入浏览器书签
+ * @param type 浏览器类型
+ */
 export const importBrowserBookmarks = async (type: BrowserType) => {
   try {
     ipcSenders.importBrowserBookmarks(type)
